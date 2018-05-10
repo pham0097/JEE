6c87c2b6d04c68c35691107e2af9fa1c80951e8d
pham0097 <pham0097@algonquinlive.com>
2018-05-09 23:59:38 -0400

    first commit

--- FishStick-war/src/java/service/ApplicationConfig.java
+++ FishStick-war/src/java/service/ApplicationConfig.java
@@ -31,7 +31,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(business.FishStickFacadeREST.class);
     }
     
 }
